9a28f9834d06dbfebb8e881196070fd0e33cdaf3
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-11-06 22:30:02 +0530

    Creating objects in typescript

---
+++ typescript_learning/myObjects.ts
@@ -0,0 +1,13 @@
+const user = {
+    name: "Alice",
+    email: "alice.alice@alice.com",
+    isActive: true
+}
+
+function createUser(user: {name: string, isPaid: boolean}){}
+
+function createCourse(): {name: string, price: number} {
+    return {name: "reactjs", price: 399}
+}
+
+export{}
