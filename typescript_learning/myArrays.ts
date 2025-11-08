e6f3b99bb761ede578947f657d1dccff46f4d10a
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-11-08 17:56:41 +0530

    Worked on arrays

---
+++ typescript_learning/myArrays.ts
@@ -0,0 +1,18 @@
+let superHeros: string[] = [];
+let heroPower: Array<number> = [];
+
+type User = {
+    name: string;
+    isActive: boolean
+}
+
+const allUsers: User[] = []
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    [0, 0]
+]
+
+superHeros.push("spiderMan");
+heroPower.push(2);
+allUsers.push({ name: "Bruce", isActive: true }, { name: "Clark", isActive: false });
