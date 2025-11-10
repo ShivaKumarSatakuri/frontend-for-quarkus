0cfb7520a509d25653dd8ce2e725ac5173621dd3
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-11-10 19:30:04 +0530

    Commented out content in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+
+// The above is the default code created by create-react-app
+// We are not using it currently as we have created our own structure
+// Hence commenting it out for now
+// We might come back to it later
+// once we are more comfortable with React basics
+// and TypeScript integration
+// We will focus on understanding components, props, state, and routing first
+// before diving back into the default App component
+// This will help us build a solid foundation for our React applications
+// and make it easier to integrate more complex features later on
+// Once we are comfortable with the basics, we can revisit this file
+// and see how we can incorporate its structure and styling into our project
+// for now, we will keep it simple and focused on learning the core concepts
+// of React and TypeScript
+export{}
