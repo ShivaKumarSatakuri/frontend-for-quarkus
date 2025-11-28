26eabb2504d5e7d5537a10f0b679f90c039d7096
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-11-28 20:04:15 +0530

    Update ResultPage.tsx

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -57,4 +57,5 @@ export default ResultPage;
 // Completed setting up mock server. Written unit tests for components
 // Worked on fixing bugs in unit tests today. Deployed changes in init env.
 // Completed one feature. UI, tests and mock server integration
-// react interview questions 
+// react interview questions
+// no progress today 
